9f3573f9289906433669dbf793c71be6bae5984c
vasyl_artus <basa94389@gmail.com>
2021-12-05 21:11:21 +0200

    Done)

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-
-let timerId
-const Timer = ({ timeStart, step ,autostart}) => {
-    const [time, setTime] = useState(timeStart);
-    const [pause, setPause] = useState(false);
-
-    useEffect(() => {
-    
-        if (!pause) {
-            console.log("Таймер запущено!");
-            timerId = setInterval(() => {
-                setTime((prev) => {
-                    if (prev > 0) {
-                        console.log(`Залишилось часу: ${prev / 1000} сек`);
-                        return prev - step
-                    }
-                     else {
-                        console.log("Час вийшов");
-                        return prev = timeStart;
-                    }
-                })
-            }, step)
-        } else {
-            console.log("Таймер на паузі!")
-            return clearInterval(timerId)
-        }
-      
-    }, [pause])
+import Timer from './components/Timer';
 
 
-    return (
-        <div>
-            <div>{`0${Math.floor(time / 1000 / 60)}`.slice(-2)}:{`0${time / 1000 % 60}`.slice(-2)}</div>
-            <button onClick={() => setPause(false)}>Start</button>
-            <button onClick={() => setPause(true)}>Stop</button>
-            <button onClick={() => setTime(timeStart)}>Reset</button>
-        </div>
 
-    )
-}
+
+ 
 
 function App() {
 
@@ -47,11 +12,13 @@ function App() {
     const timeStart_2 = 1000 * 20;
     const step_1 = 1000
     const step_2 = 2000
-     const autostart = true;
+    const autostart = true;
+    const infiniti = false;
+    const onTick=(time) => console.log("Залишилось часу: " + time/1000)
     return (
         <div className="App">
-            <Timer timeStart={timeStart_1} step={step_1} autostart ={autostart} />
-            <Timer timeStart={timeStart_2} step={step_2} autostart ={autostart} />
+            <Timer timeStart={timeStart_1} step={step_1} autostart ={autostart} onTick={onTick} infiniti={infiniti} />
+            <Timer timeStart={timeStart_2} step={step_2} autostart ={autostart}  onTick={onTick}  infiniti={infiniti} />
         </div>
     );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+const Timer = ({ timeStart, step , onTick, autostart , infiniti}) => {
+    const [time, setTime] = useState(timeStart);
+    const [pause, setPause] = useState(!autostart);
+    const [repetition, setRepetition] = useState(infiniti);
+
+    const handleResetTimer = () => {
+        setPause(!autostart)
+        setTime(timeStart);
+    }
+   
+    const handleRepeatTimer = () => {
+        if(repetition){
+            setRepetition(false)
+            setTime(timeStart)
+            setPause(false)
+            
+        }else{
+            setRepetition(true)
+            setTime(timeStart);
+            setPause(false)
+        }
+    };
+
+    useEffect(() => {
+        if(repetition){
+            if(!pause) {
+                const interval = setInterval(() => {
+                    setTime((prev) => {
+                          if (prev > 0) {
+                              return prev - step
+                          }else {
+                              return prev = timeStart;
+                          }
+                      })
+                      onTick(time)
+                   }, step)
+                   
+                   return () => clearInterval(interval)  
+        }
+            
+        }else{
+            if(!pause) {
+                const interval = setInterval(() => {
+                   setTime((prev) => prev - step)
+                   onTick(time)
+                }, step)
+                if(time === 0) {
+                  setPause(true)
+                }
+                return () => clearInterval(interval)
+        }
+        
+     }},[time, pause, repetition ])
+ 
+
+
+    return (
+        <div>
+            <div>{`0${Math.floor(time / 1000 / 60)}`.slice(-2)}:{`0${time / 1000 % 60}`.slice(-2)}</div>
+            <button onClick={() => setPause(false)}>Start</button>
+            <button onClick={() => setPause(true)}>Stop</button>
+            <button onClick={handleResetTimer}>Reset</button>
+            <button onClick={handleRepeatTimer}>Repeat</button>
+        </div>
+
+    )
+}
+
+export default Timer
