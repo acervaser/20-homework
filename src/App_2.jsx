48923a083fd81978964187b45490e4de9146a1a6
vasyl_artus <basa94389@gmail.com>
2021-12-05 12:04:38 +0200

    almost done+

---
+++ src/App_2.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+let timerId
+const Timer = ({ timeStart, step ,autostart}) => {
+    const initpause = true
+    const [time, setTime] = useState(timeStart);
+    const [pause, setPause] = useState(false);
+
+    useEffect(() => {
+    
+        if (!pause) {
+            timerId = setInterval(() => {
+                setTime((prev) => {
+                    if (prev > 0) {
+                        console.log(`Залишилось часу: ${prev / 1000} сек`);
+                        return prev - step
+                    } else {
+                        console.log("Час вийшов");
+                        return prev = timeStart;
+                    }
+                })
+            }, 1000)
+        } else {
+            return clearInterval(timerId)
+        }
+      
+    }, [pause])
+
+
+    return (
+        <div>
+            <div>{`0${Math.floor(time / 1000 / 60)}`.slice(-2)}:{`0${time / 1000 % 60}`.slice(-2)}</div>
+            <button className="button start" onClick={() => setPause(false)}>Start</button>
+            <button className="button stop" onClick={() => setPause(true)}>Stop</button>
+            <button className="button reset" onClick={() => setTime(timeStart)}>Reset</button>
+        </div>
+
+    )
+}
+
+function App() {
+
+    const timeStart_1 = 1000 * 10;
+    const timeStart_2 = 1000 * 20;
+    const step_1 = 1000
+    const step_2 = 2000
+     const autostart = true;
+    return (
+        <div className="App">
+            <Timer timeStart={timeStart_1} step={step_1} autostart ={autostart} />
+            <Timer timeStart={timeStart_2} step={step_2} autostart ={autostart} />
+        </div>
+    );
+}
+
+export default App;
