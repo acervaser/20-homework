30e5b3ac41c8672add3e5b2f381fdbd0e3f321bd
vasyl_artus <basa94389@gmail.com>
2021-12-05 21:25:56 +0200

    text repair

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from 'react';
-const Timer = ({ timeStart, step , onTick, autostart , infiniti}) => {
+const Timer = ({ timeStart, step, onTick, autostart, infiniti }) => {
     const [time, setTime] = useState(timeStart);
     const [pause, setPause] = useState(!autostart);
     const [repetition, setRepetition] = useState(infiniti);
@@ -9,14 +9,14 @@ const Timer = ({ timeStart, step , onTick, autostart , infiniti}) => {
         setPause(!autostart)
         setTime(timeStart);
     }
-   
+
     const handleRepeatTimer = () => {
-        if(repetition){
+        if (repetition) {
             setRepetition(false)
             setTime(timeStart)
             setPause(false)
-            
-        }else{
+
+        } else {
             setRepetition(true)
             setTime(timeStart);
             setPause(false)
@@ -24,42 +24,46 @@ const Timer = ({ timeStart, step , onTick, autostart , infiniti}) => {
     };
 
     useEffect(() => {
-        if(repetition){
-            if(!pause) {
+        if (repetition) {
+            if (!pause) {
                 const interval = setInterval(() => {
                     setTime((prev) => {
-                          if (prev > 0) {
-                              return prev - step
-                          }else {
-                              return prev = timeStart;
-                          }
-                      })
-                      onTick(time)
-                   }, step)
-                   
-                   return () => clearInterval(interval)  
-        }
-            
-        }else{
-            if(!pause) {
-                const interval = setInterval(() => {
-                   setTime((prev) => prev - step)
-                   onTick(time)
+                        if (prev > 0) {
+                            return prev - step
+                        } else {
+                            return prev = timeStart;
+                        }
+                    })
+                    onTick(time)
                 }, step)
-                if(time === 0) {
-                  setPause(true)
+
+                return () => clearInterval(interval)
+            }
+
+        } else {
+            if (!pause) {
+                const interval = setInterval(() => {
+                    setTime((prev) => prev - step)
+                    onTick(time)
+                }, step)
+                if (time === 0) {
+                    setPause(true)
                 }
                 return () => clearInterval(interval)
+            }
+
         }
-        
-     }},[time, pause, repetition ])
- 
+    }, [time, pause, repetition])
+
 
 
     return (
         <div>
-            <div>{`0${Math.floor(time / 1000 / 60)}`.slice(-2)}:{`0${time / 1000 % 60}`.slice(-2)}</div>
-            <p>{repetition && "Infiniti mod on"}</p>
+
+            <div>
+                <p>{repetition && "Infiniti mod on"}</p>
+                {`0${Math.floor(time / 1000 / 60)}`.slice(-2)}:{`0${time / 1000 % 60}`.slice(-2)}
+            </div>
             <button onClick={() => setPause(false)}>Start</button>
             <button onClick={() => setPause(true)}>Stop</button>
             <button onClick={handleResetTimer}>Reset</button>
